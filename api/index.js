ea3eeb9424e3657bd2dd84088c69b1bde698ddf8
lucaimbalzano <lucaimbalzano@gmail.com>
2024-03-16 14:22:47 +0100

    mongodb connected

--- api/index.js
+++ api/index.js
@@ -1,6 +1,14 @@
 import express from 'express';
+import mongoose  from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('mongodb connected')
+}).catch((err) => console.log(err));
 
 const app = express();
 app.listen(3000,()=>{
-    console.log('server on 3000@!')
+    console.log('server on 30ds00dcsd@!')
 });
