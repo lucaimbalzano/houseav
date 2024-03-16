e7ca0b6e31fcadad2e4a198115e1051d1c94a700
lucaimbalzano <lucaimbalzano@gmail.com>
2024-03-16 17:07:48 +0100

    complete signup

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose  from 'mongoose';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -10,8 +11,21 @@ mongoose.connect(process.env.MONGO).then(()=>{
 }).catch((err) => console.log(err));
 
 const app = express();
+app.use(express.json());
+
 app.listen(3000,()=>{
     console.log('server on 30ds00dcsd@!')
 });
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    });
+})
