b457d3f07637615940307b7d2bc18b9c93746aa7
lucaimbalzano <lucaimbalzano@gmail.com>
2024-03-23 13:24:18 +0100

    added states and upload files functions

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,6 +1,22 @@
 import React, { useState } from "react";
+import { firebaseApp } from "../firebase.js";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
 
 export default function CreateListing() {
+  const [imageUploadError, setImageUploadError] = useState(false);
+  const [uploading, setUploading] = useState(false);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const [files, setFiles] = useState([]);
+  var currentTime = new Date().getTime();
+  var oneWeek = 7 * 24 * 60 * 60 * 1000;
+  var currentTimePlusWeek = currentTime + oneWeek;
   const [formData, setFormData] = useState({
     imageUrls: [],
     name: "",
@@ -14,12 +30,83 @@ export default function CreateListing() {
     offer: false,
     parking: false,
     furnished: false,
+    availability: true,
+    availabilityDateStartFrom: currentTime,
+    availabilityDateEndOn: currentTimePlusWeek,
+    sleepPlace: 1,
   });
 
   const handleChange = () => {};
 
-  const handleSubmit = () => {};
+  const handleImageSubmit = (event) => {
+    if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
+      setUploading(true);
+      setImageUploadError(false);
+      const promises = [];
 
+      for (let i = 0; i < files.length; i++) {
+        promises.push(storeImageUrls(files[i]));
+      }
+      // I use Proimise because i have multiple asynchronous operations that can be executed concurrently, i wait all of them to complete before continuing.
+      Promise.all(promises)
+        .then((urls) => {
+          setFormData({
+            ...formData,
+            imageUrls: formData.imageUrls.concat(urls),
+          });
+          setImageUploadError(false);
+          setUploading(false);
+        })
+        .catch((err) => {
+          console.log(err.stack);
+          setImageUploadError("Image upload failed (2 mb max per image)");
+          setUploading(false);
+        });
+    } else {
+      setImageUploadError("You can only upload 6 images per listing");
+      setUploading(false);
+    }
+  };
+
+  const storeImageUrls = async (file) => {
+    if (file.size > 2 * 1024 * 1024) {
+      console.log("ERROR----------------");
+      throw new Error("File exceeds maximum size (2MB)");
+    }
+    return new Promise((resolve, reject) => {
+      const storage = getStorage(firebaseApp);
+      const fileName = new Date().getTime() + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log(`Upload is ${progress}% done`);
+        },
+        (error) => {
+          reject(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            resolve(downloadURL);
+          });
+        }
+      );
+    });
+  };
+
+  const handleRemoveImage = (index) => {
+    setFormData({
+      ...formData,
+      imageUrls: formData.imageUrls.filter(
+        (item, indexUrls) => indexUrls != index
+      ),
+    });
+  };
+
+  const handleSubmit = () => {};
   return (
     <main className="p-3 max-w-4xl mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">
@@ -159,67 +246,64 @@ export default function CreateListing() {
                 <p>Baths</p>
               </div>
             </div>
-
-            <div className="flex flex-col flex-1 gap-4">
-              <p className="font-semibold">
-                Images:
-                <span className="font-normal text-gray-600 ml-2">
-                  The first image will be the cover (max 6)
-                </span>
-              </p>
-              <div className="flex gap-4">
-                <input
-                  onChange={(e) => setFiles(e.target.files)}
-                  className="p-3 border border-gray-300 rounded w-full"
-                  type="file"
-                  id="images"
-                  accept="image/*"
-                  multiple
+          </div>
+        </div>
+        <div className="flex flex-col flex-1 gap-4">
+          <p className="font-semibold">
+            Images:
+            <span className="font-normal text-gray-600 ml-2">
+              The first image will be the cover (max 6)
+            </span>
+          </p>
+          <div className="flex gap-4">
+            <input
+              onChange={(e) => setFiles(e.target.files)}
+              className="p-3 border border-gray-300 rounded w-full"
+              type="file"
+              id="images"
+              accept="image/*"
+              multiple
+            />
+            <button
+              type="button"
+              disabled={uploading}
+              onClick={handleImageSubmit}
+              className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80"
+            >
+              {uploading ? "Uploading..." : "Upload"}
+            </button>
+          </div>
+          <p className="text-red-700 text-sm">
+            {imageUploadError && imageUploadError}
+          </p>
+          {formData.imageUrls.length > 0 &&
+            formData.imageUrls.map((url, index) => (
+              <div
+                key={url}
+                className="flex justify-between p-3 border items-center"
+              >
+                <img
+                  src={url}
+                  alt="listing image"
+                  className="w-40 h-40 sm:w-20 sm:h-20 object-contain rounded-lg"
                 />
                 <button
                   type="button"
-                  //   disabled={uploading}
-                  //   onClick={handleImageSubmit}
-                  className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80"
+                  onClick={() => handleRemoveImage(index)}
+                  className="p-3 text-red-700 rounded-lg uppercase hover:opacity-75"
                 >
-                  {/* {uploading ? "Uploading..." : "Upload"} */}
-                  'upload'
+                  Delete
                 </button>
               </div>
-              <p className="text-red-700 text-sm">
-                {/* {imageUploadError && imageUploadError} */}
-                imageUploadError
-              </p>
-              {formData.imageUrls.length > 0 &&
-                formData.imageUrls.map((url, index) => (
-                  <div
-                    key={url}
-                    className="flex justify-between p-3 border items-center"
-                  >
-                    <img
-                      src={url}
-                      alt="listing image"
-                      className="w-20 h-20 object-contain rounded-lg"
-                    />
-                    <button
-                      type="button"
-                      onClick={() => handleRemoveImage(index)}
-                      className="p-3 text-red-700 rounded-lg uppercase hover:opacity-75"
-                    >
-                      Delete
-                    </button>
-                  </div>
-                ))}
-              <button
-                // disabled={loading || uploading}
-                className="p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
-              >
-                {/* {loading ? "Creating..." : "Create listing"} */}
-                Create Listing
-              </button>
-              {/* {error && <p className="text-red-700 text-sm">{error}</p>} */}
-            </div>
-          </div>
+            ))}
+          <button
+            // disabled={loading || uploading}
+            className="p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+          >
+            {/* {loading ? "Creating..." : "Create listing"} */}
+            Create Listing
+          </button>
+          {error && <p className="text-red-700 text-sm">{error}</p>}
         </div>
       </form>
     </main>
